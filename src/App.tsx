d2a43706c7f0b64a94c1bc3cc88523d8ba883120
zzzjizon <flashgift@naver.com>
2022-10-09 00:34:38 +0900

    add grid enable

--- src/App.tsx
+++ src/App.tsx
@@ -1,104 +1,18 @@
-import React, { useEffect } from 'react'
-import p5 from 'p5'
+import React, { useState, useEffect } from 'react'
+import P5Component from './component/P5Component/P5Component'
 
 
 
 
-const s = (sketch:p5) => {
 
-  let scale:number = 12;
-  let graphicRenderer:p5;
-
-  const SetGrid = (gridCount:number) => {
-    let i:number = 0, j:number = 0;
-    sketch.strokeWeight(0.7);
-
-    if(true) {
-      for(j = 0; j < graphicRenderer.width/gridCount; j++) {
-        sketch.stroke(0, 40)
-        for(i = 1;i < gridCount; i++) {
-          sketch.line(0.5 + (i + j * gridCount) * scale, 0.5, 0.5 + (i + j * gridCount) * scale, 0.5 + graphicRenderer.height * scale);
-        }
-        sketch.stroke(0, 255)
-        sketch.line(0.5 + (gridCount + j * gridCount) * scale, 0.5, 0.5 + (gridCount + j * gridCount) * scale, 0.5 + graphicRenderer.height * scale);
-      }
-  
-      
-      for(j = 0; j < graphicRenderer.height/gridCount; j++) {
-        sketch.stroke(0, 40)
-        for(i = 1;i < gridCount; i++) {
-          sketch.line(0.5, 0.5 + (i + j * gridCount) * scale, 0.5 + graphicRenderer.width * scale, 0.5 + (i + j * gridCount) * scale);
-        }
-        sketch.stroke(0, 255)
-        sketch.line(0.5, 0.5 + (gridCount + j * gridCount) * scale, 0.5 + graphicRenderer.width * scale, 0.5 + (gridCount + j * gridCount) * scale);
-      }
-    }
-    
-    
-    sketch.fill(0,0,0,0)
-    sketch.rect(0.5, 0.5, graphicRenderer.width * scale, graphicRenderer.height * scale)
-  }
-
-  sketch.setup = () => {
-    sketch.createCanvas(1024, 768, 'p2d')
-    graphicRenderer = sketch.createGraphics(128, 128, 'p2d')
-    sketch.background(0, 155, 55)
-    sketch.noSmooth()
-    graphicRenderer.noSmooth()
-    graphicRenderer.strokeWeight(1)
-    graphicRenderer.stroke(100, 255)
-  }
-  
-  sketch.draw = () => {
-    sketch.background(255, 255, 255);
-
-    sketch.image(graphicRenderer as unknown as p5.Image, 0.5, 0.5, graphicRenderer.width * scale, graphicRenderer.height * scale)
-    
-    SetGrid(8)
-  }
-  
-  sketch.mouseDragged = () => {
-    let mx:number, my:number;
-
-    if(sketch.mouseX < 0)
-      return
-    else if(sketch.mouseX > graphicRenderer.width * scale)
-      return
-    else
-      mx = sketch.mouseX;
-
-    if(sketch.mouseY < 0)
-      return
-    else if(sketch.mouseY > graphicRenderer.height * scale)
-      return
-    else
-      my = sketch.mouseY;
-
-    graphicRenderer.loadPixels()
-    switch(sketch.mouseButton){
-      case 'right':
-        graphicRenderer.set(sketch.int(mx / scale), sketch.int(my / scale), 255)
-        break
-      case 'left':
-        graphicRenderer.set(sketch.int(mx / scale), sketch.int(my / scale), 0)
-        break
-    }
-    graphicRenderer.updatePixels()
-  }
-  sketch.mousePressed = sketch.mouseDragged
-}
-
-
-let p5Instance:p5;
 
 const App:React.FC = () => { 
-  
+
+  const [gridDraw, SetGridDraw] = useState<boolean>(true)
+
   useEffect(() => {
-    p5Instance = new p5(s, document.getElementById('p5Render') as HTMLElement)
-    return () => {
-      p5Instance = null
-    }
-  }, []);
+
+  }, [gridDraw])
 
   return <div style={{display: 'inline-flex', flexDirection: 'row', justifyContent: 'center'}}>
               <div style={{border: '1px solid black', width:'350px'}}>
@@ -106,9 +20,10 @@ const App:React.FC = () => {
                 1.마우스휠로 확대 축소 가능<br></br>
                 2.왼쪽클릭으로 드래그하면 그리기 가능<br></br>
                 3.오른쪽클릭으로 드래그하면 지우기 가능<br></br>
+                <input type='checkbox' id='gridDrawCheckbox' onChange={(e) => {SetGridDraw(e.target.checked)}}/>
               </div>
               <div style={{border: '1px solid black', width:'1280px', display: 'inline-flex', justifyContent:'center', alignItems:'center'}} >
-                <div id = 'p5Render'></div>
+                <P5Component name='asdf' gridDraw = {gridDraw}/>
               </div>
               <div>
               </div>
